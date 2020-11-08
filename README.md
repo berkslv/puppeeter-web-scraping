2568f13d470b8cfedaa0bbe0ffc4cd0700330fa6
berk selvi <berkslv@gmail.com>
2020-11-08 14:06:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Puppeter for Web scraping
+This small project scrapes a stock price, processes the data and mails the results to a specific user.
+Turkish article of this project is: "medium-url"
--- app.js
+++ app.js
@@ -2,24 +2,12 @@ let puppeteer = require('puppeteer');
 var nodemailer = require('nodemailer');
 
 scrapePrice('https://www.google.com/search?client=opera&hs=Wzk&sxsrf=ALeKk00I7IW96h0D13mdL99WUos0Xhz5MA%3A1604829566135&ei=fsGnX_DmB4XosAeQpInwAQ&q=türk+hava+yolları+hisse&oq=türk+hava+yolları+hisse&gs_lcp=CgZwc3ktYWIQAzIICAAQyQMQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywE6BAgAEEc6CwguEMcBEK8BEMsBUMULWKYSYNcSaABwAngAgAGpAogBwAeSAQUwLjQuMZgBAKABAaoBB2d3cy13aXrIAQjAAQE&sclient=psy-ab&ved=0ahUKEwjwnduB2PLsAhUFNOwKHRBSAh4Q4dUDCAw&uact=5');
-async function scrapePrice(url){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-
-    const [el] = await page.$x('//*[@id="knowledge-finance-wholepage__entity-summary"]/div/g-card-section/div/g-card-section/span[1]/span/span[1]');
-    const txt = await (await el.getProperty('textContent')).jsonValue(); // return 9,71 but parseFloat need to period not comma
-    browser.close();
-    let stringTxt = JSON.stringify(txt).replace('"',"").replace('"',"").replace(",","."); // first change json to string type, this return "number.decimal" and some string methods
-    console.log(stringTxt);
-    main(parseFloat(stringTxt));
-}
 
 function main(srcTxt)
 {
     
     let currentPrice = srcTxt;
-    let myPrice = 0.079226;
+    let myPrice = 9.62;
     let myAmount = 6800;
 
     const currentEarningPercent = calcEarningPercent(currentPrice,myPrice);
@@ -27,6 +15,20 @@ function main(srcTxt)
     const currentEarningProfit = calcEarningProfit(currentEarningAmount,myAmount);
 
     sendMail(currentEarningPercent,parseInt(currentEarningAmount),parseInt(currentEarningProfit));
+}
+
+async function scrapePrice(url){
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x('//*[@id="knowledge-finance-wholepage__entity-summary"]/div/g-card-section/div/g-card-section/span[1]/span/span[1]');
+  const txt = await (await el.getProperty('textContent')).jsonValue(); // return 9,71 but parseFloat need to period not comma
+  browser.close();
+  let stringTxt = JSON.stringify(txt).replace('"',"").replace('"',"").replace(",","."); // first change json to string type, this return "number.decimal" and some string methods
+  console.log(stringTxt);
+  
+  main(parseFloat(stringTxt));
 }
 
 function calcEarningPercent(currentPrice,yourPrice){
@@ -44,22 +46,22 @@ async function sendMail(Percent,Amount,Profit){
         service: 'gmail',
         auth: {
           user: 'berkslv.bot@gmail.com',
-          pass: 'l5tkmn16Bsh113.'
+          pass: '<berslv.bot - password>'
         }
       });
       
       var mailOptions = {
         from: 'berkslv.bot@gmail.com',
         to: 'berkslv@gmail.com',
-        subject: 'Günün kazancı',
+        subject: 'Earnings of the day',
         html: `
-                Kar Oranı: <h3 style="display: inline; font-family: Arial, Helvetica, sans-serif;">${Percent} %</h3>
+                Profit percent: <h3 style="display: inline; font-family: Arial, Helvetica, sans-serif;">${Percent} %</h3>
             <br>
             <hr>
-                Kar: <h3 style="display: inline; font-family: Arial, Helvetica, sans-serif;">${Amount} &#8378;</h3>
+                Profit: <h3 style="display: inline; font-family: Arial, Helvetica, sans-serif;">${Amount} &#8378;</h3>
             <br>
             <hr>
-                Güncel miktar: <h3 style="display: inline; font-family: Arial, Helvetica, sans-serif;">${Profit} &#8378;</h3>
+                Current amount: <h3 style="display: inline; font-family: Arial, Helvetica, sans-serif;">${Profit} &#8378;</h3>
         `
       };
       
